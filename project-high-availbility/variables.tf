dcfcf9bef9c24c5fc39bba12f51c762a4b2b3719
mayankpatel1 <146357011+mayankpatel1@users.noreply.github.com>
2023-10-16 09:56:46 +0100

    Update variables.tf

--- project-high-availbility/variables.tf
+++ project-high-availbility/variables.tf
@@ -1,48 +1,29 @@
+# variables.tf
+
 variable "tags" {
-  default = {
-    Name = "high-availability"
-    
-  }
+  type = map
 }
 
 variable "igw_tags" {
-  default = {
-    Name = "high-availability-igw" 
-    
-  }
+  type = map
 }
 
 variable "public_subnet_tags" {
-  default = {
-    Name = "high-availability"
-    
-  }
+  type = map
 }
 
 variable "private_subnet_tags" {
   type = map
-  default = {
-    Name = "high-availability-private-subnet"
-    
-  }
 }
 
 variable "public_route_table_tags" {
   type = map
-  default = {
-    Name = "high-availability-public-rt"
-  }
 }
 
 variable "nat_tags" {
   type = map
-  default = {
-    Name = "high-availability-natgatway"
 }
 
 variable "private_route_table_tags" {
   type = map
-  default = {
-    Name = "high-availability-private-rt"
-  }
 }
