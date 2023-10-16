b140cee4571ead91db27d5efe9103a4138eb9786
mayankpatel1 <146357011+mayankpatel1@users.noreply.github.com>
2023-10-16 10:22:33 +0100

    Update main.tf

--- project-high-availbility/main.tf
+++ project-high-availbility/main.tf
@@ -30,28 +30,4 @@ module "vpc" {
   }
   
   
-  resource "aws_security_group" "web" {
-    name        = "allow_tls"
-    description = "Allow TLS inbound traffic"
-    vpc_id      = module.vpc.vpc_id
   
-    ingress {
-      description      = "TLS from VPC"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-  
-    tags = {
-      Name = "timing-web"
-    }
-  }
